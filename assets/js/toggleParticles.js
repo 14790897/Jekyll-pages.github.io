3044c4f22ce00aa9b708cc455a8988142746a376
14790897 <14790897abc@gmail.com>
2023-04-20 13:37:18 +0800

    Add

--- assets/js/toggleParticles.js
+++ assets/js/toggleParticles.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   var particlesContainer = document.getElementById('particles-js');
-  particlesContainer.classList.add('hidden'); // 初始化时隐藏粒子效果
+  // particlesContainer.classList.add('hidden'); // 初始化时隐藏粒子效果
   var toggleParticlesButton = document.getElementById('toggleParticles');
 
   toggleParticlesButton.addEventListener('click', function () {
     if (particlesContainer.classList.contains('hidden')) {
       particlesContainer.classList.remove('hidden');
       toggleParticlesButton.textContent = '关闭粒子效果';
-    } else {
+    } 
+    else {
       particlesContainer.classList.add('hidden');
       toggleParticlesButton.textContent = '开启粒子效果';
     }
