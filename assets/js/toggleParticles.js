e118fa6829ea5cc0d977bd089742a76f65430ec9
14790897 <14790897abc@gmail.com>
2023-04-20 09:11:24 +0800

    Add

--- assets/js/toggleParticles.js
+++ assets/js/toggleParticles.js
@@ -1,20 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var particlesEnabled = false;
-    var particlesToggleBtn = document.getElementById("toggleParticles");
-    var particlesContainer = document.getElementById("particles-js");
-  
-    particlesToggleBtn.addEventListener("click", function () {
-      particlesEnabled = !particlesEnabled;
-  
-      if (particlesEnabled) {
-        particlesToggleBtn.textContent = "关闭粒子效果";
-        particlesContainer.style.display = "block";
-      } else {
-        particlesToggleBtn.textContent = "开启粒子效果";
-        particlesContainer.style.display = "none";
-      }
-    });
-  
-    particlesContainer.style.display = "none";
+document.addEventListener('DOMContentLoaded', function () {
+  var particlesContainer = document.getElementById('particles-js');
+  particlesContainer.classList.add('hidden'); // 初始化时隐藏粒子效果
+  var toggleParticlesButton = document.getElementById('toggleParticles');
+
+  toggleParticlesButton.addEventListener('click', function () {
+    if (particlesContainer.classList.contains('hidden')) {
+      particlesContainer.classList.remove('hidden');
+      toggleParticlesButton.textContent = '关闭粒子效果';
+    } else {
+      particlesContainer.classList.add('hidden');
+      toggleParticlesButton.textContent = '开启粒子效果';
+    }
   });
-  
+});
