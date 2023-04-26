58ead75f2937c7515466ddfdd3c8a213c1113cc7
14790897 <14790897abc@gmail.com>
2023-04-26 21:22:31 +0800

    Add

--- assets/js/intro-guide.js
+++ assets/js/intro-guide.js
@@ -1,13 +1,15 @@
 // 在 intro-guide.js 文件中
 document.addEventListener('DOMContentLoaded', function () {
   const introjs_done = getCookie('introjs_done');
-  if (introjs_done === null) {
+  const isIntroDone = localStorage.getItem('introjs-done');
+  if (introjs_done === null || isIntroDone === null) {
     introJs()
       .setOptions({
         // ... 其他选项
       })
       .onexit(function() {
         setCookie('introjs_done', 'true', 365);
+        localStorage.setItem('introjs-done', 'true');
       })
       .start();
   }
