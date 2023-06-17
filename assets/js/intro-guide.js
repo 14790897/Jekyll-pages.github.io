734adeb49aaeb574a317ac1523f50c0ff86247d0
14790897 <14790897abc@gmail.com>
2023-06-17 20:45:14 +0800

    Update intro-guide.js

--- assets/js/intro-guide.js
+++ assets/js/intro-guide.js
@@ -17,19 +17,19 @@ function getCookie(name) {
   return null;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const introjs_done = getCookie('introjs_done');
-  const isIntroDone = localStorage.getItem('introjs-done');
-  if (introjs_done === null && isIntroDone === null) {
-    introJs()
-      .setOptions({
-        // ... 其他选项
-      })
-      .onexit(function() {
-        setCookie('introjs_done', 'true', 365);
-        localStorage.setItem('introjs-done', 'true');
-      })
-      .start();
-  }
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   const introjs_done = getCookie('introjs_done');
+//   const isIntroDone = localStorage.getItem('introjs-done');
+//   if (introjs_done === null && isIntroDone === null) {
+//     introJs()
+//       .setOptions({
+//         // ... 其他选项
+//       })
+//       .onexit(function() {
+//         setCookie('introjs_done', 'true', 365);
+//         localStorage.setItem('introjs-done', 'true');
+//       })
+//       .start();
+//   }
+// });
 
